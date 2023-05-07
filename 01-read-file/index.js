882cd633d9386fbe5c76057b10d7a428c1e7d185
unknown <besssta888@mail.ru>
2023-05-07 15:31:45 +0300

    refactor: fix eslint mistake

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const path = require('path');
 const readableStream = fs.createReadStream('01-read-file/text.txt', 'utf-8');
 
 readableStream.on('data', text => console.log(text));
