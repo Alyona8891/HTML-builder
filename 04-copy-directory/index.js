a1c6d172b90595fd5e2b0e88a1af48f786e89246
unknown <besssta888@mail.ru>
2023-05-09 04:41:30 +0300

    feat: add solution with async functions

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,29 +1,60 @@
 const fsPr = require('fs').promises;
-const fs = require('fs');
 const path = require('path');
-const newPath = `${path.dirname(__dirname)}/04-copy-directory/files`;
 
-fsPr.mkdir(
-  path.join(__dirname, 'files-copy'),
-  {
-    recursive: true
-  },
-  err => {
-    if(err) {
-      throw err;
+async function copyDir() {
+
+  async function delFolder(src) {
+    try {
+      await fsPr.rm(src, {recursive: true});
+    } catch(err) {
+      console.log(err);
     }
-  });
-
-fs.readdir(newPath, (err, files) => {
-  if(err) {
-    throw err;
-  } else {
-    files.forEach(file => {
-      fs.copyFile(`04-copy-directory/files/${file}`, `04-copy-directory/files-copy/${file}`, (err) => {
-        if(err) {
-          throw err;
-        }
-      });
-    });
   }
-});
+
+  async function createFolder(src) {
+    try {
+      await fsPr.mkdir(src, {recursive: true});
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  
+  async function copyFiles(currentSrc, copySrc) {
+    try {
+      const files = await fsPr.readdir(currentSrc);
+      for(const file of files) {
+        const newCurrentSrc = path.join(currentSrc, file);
+        const newCopySrc = path.join(copySrc, file);
+        try {
+          const stats = await fsPr.stat(newCurrentSrc);
+          if (stats.isFile()) {
+            try {
+              await fsPr.copyFile(newCurrentSrc, newCopySrc);
+            } catch(err) {
+              console.log(err);
+            }
+          } else {
+            await createFolder(newCopySrc);
+            await copyFiles(newCurrentSrc, newCopySrc);
+          }
+        } catch(err) {
+          console.log(err);
+        }  
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  try {
+    await fsPr.access(path.join(__dirname, 'files-copy'));
+    await delFolder(path.join(__dirname, 'files-copy'));
+    await createFolder(path.join(__dirname, 'files-copy'));
+  } catch(err) {
+    await createFolder(path.join(__dirname, 'files-copy'));
+  }
+
+  await copyFiles(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'));
+}
+  
+copyDir();
